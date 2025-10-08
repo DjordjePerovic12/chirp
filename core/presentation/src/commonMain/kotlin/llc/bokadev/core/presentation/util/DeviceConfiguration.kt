34b045a0d355e85e616256e8c2996a415daab80b
Djordje Perovic <djordje.perovic@amplitudo.me>
2025-10-08 15:52:57 +0200

    Adds currentDeviceConfiguration composable util

--- core/presentation/src/commonMain/kotlin/llc/bokadev/core/presentation/util/DeviceConfiguration.kt
+++ core/presentation/src/commonMain/kotlin/llc/bokadev/core/presentation/util/DeviceConfiguration.kt
@@ -1,10 +1,19 @@
 package llc.bokadev.core.presentation.util
 
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
+import androidx.compose.runtime.Composable
 import androidx.window.core.layout.WindowSizeClass
 import androidx.window.core.layout.WindowSizeClass.Companion.HEIGHT_DP_EXPANDED_LOWER_BOUND
 import androidx.window.core.layout.WindowSizeClass.Companion.HEIGHT_DP_MEDIUM_LOWER_BOUND
 import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_EXPANDED_LOWER_BOUND
 import androidx.window.core.layout.WindowSizeClass.Companion.WIDTH_DP_MEDIUM_LOWER_BOUND
+
+
+@Composable
+fun currentDeviceConfiguration(): DeviceConfiguration {
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+    return DeviceConfiguration.fromWindowSizeClass(windowSizeClass)
+}
 
 enum class DeviceConfiguration {
     MOBILE_PORTRAIT,
