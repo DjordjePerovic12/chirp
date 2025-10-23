60af305ada4b880bc4b1dca1f365907a9ec84850
Djordje Perovic <djordje.perovic@amplitudo.me>
2025-10-23 12:35:41 +0200

    Adds bottom sheet content wrapper

---
+++ core/designsystem/src/commonMain/kotlin/llc/bokadev/core/designsystem/components/dialogs/ChirpBottomSheet.kt
@@ -0,0 +1,41 @@
+package llc.bokadev.core.designsystem.components.dialogs
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.rememberModalBottomSheetState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import llc.bokadev.core.designsystem.theme.ChirpTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ChirpBottomSheet(
+    onDismiss: () -> Unit,
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = true
+    )
+
+    LaunchedEffect(sheetState.isVisible) {
+        if (sheetState.isVisible) {
+            sheetState.expand()
+        }
+    }
+
+
+    ModalBottomSheet(
+        onDismissRequest = onDismiss,
+        dragHandle = null,
+        contentWindowInsets = { WindowInsets() },
+        modifier = modifier
+            .statusBarsPadding(),
+    ) {
+        content()
+    }
+}
