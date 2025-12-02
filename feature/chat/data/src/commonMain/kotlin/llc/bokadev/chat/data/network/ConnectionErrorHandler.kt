98892de225f5259c4bc3dea5ea9390bd2ea201fb
Djordje Perovic <djordje.perovic@amplitudo.me>
2025-12-02 08:55:55 +0100

    Adds connection error and retry handlers

---
+++ feature/chat/data/src/commonMain/kotlin/llc/bokadev/chat/data/network/ConnectionErrorHandler.kt
@@ -0,0 +1,9 @@
+package llc.bokadev.chat.data.network
+
+import llc.bokadev.chat.domain.models.ConnectionState
+
+expect class ConnectionErrorHandler {
+    fun getConnectionStateForError(cause: Throwable): ConnectionState
+    fun transformException(exception: Throwable): Throwable
+    fun isRetriableError(cause: Throwable): Boolean
+}
